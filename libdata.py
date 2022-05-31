bc0f3b196369784938697deef5625c7bd0e2166e
Lim Heo <huhlim@gmail.com>
2022-05-31 12:25:27 -0400

    Applied black

--- libdata.py
+++ libdata.py
@@ -10,16 +10,17 @@ import numpy as np
 
 import torch_geometric
 
-ELEMENTs = ['H', 'C', 'N', 'O', 'S']
+ELEMENTs = ["H", "C", "N", "O", "S"]
 one_hot_encoding = lambda x: np.eye(len(ELEMENTs), dtype=np.float)[x]
 dtype = torch.get_default_dtype()
 
+
 class AAset(torch_geometric.data.Dataset):
-    def __init__(self, selection='element != H'):
+    def __init__(self, selection="element != H"):
         self.pdb_s = []
         pdb_fn_s = sorted(glob.glob("pdb/*.pdb"))
         self.n_pdb = len(pdb_fn_s)
-        for pdbIndex,pdb_fn in enumerate(pdb_fn_s):
+        for pdbIndex, pdb_fn in enumerate(pdb_fn_s):
             pdb = mdtraj.load(pdb_fn, standard_names=False)
             xyz = pdb.xyz[0]
             #
@@ -32,34 +33,43 @@ class AAset(torch_geometric.data.Dataset):
             #
             inputIndex = top.select(selection)
             #
-            data = torch_geometric.data.Data(pos=torch.tensor(xyz[inputIndex], dtype=dtype))
+            data = torch_geometric.data.Data(
+                pos=torch.tensor(xyz[inputIndex], dtype=dtype)
+            )
             data.input_element = torch.tensor(element[inputIndex], dtype=dtype)
             data.output_element = torch.tensor(element, dtype=dtype)
             data.output_xyz = torch.tensor(xyz, dtype=dtype)
             data.output_pdbIndex = pdbIndex
             #
-            print (pdbIndex, top.residue(0).name)
+            print(pdbIndex, top.residue(0).name)
             self.pdb_s.append(data)
+
     def __len__(self):
         return self.n_pdb
+
     def __getitem__(self, index):
         return self.pdb_s[index]
 
+
 def batch_to_device(batch, device, ignore=[]):
     for key in batch.keys:
-        if key in ignore: continue
+        if key in ignore:
+            continue
         batch[key] = batch[key].to(device)
     return batch
 
+
 def main():
-    dataloader = torch_geometric.data.DataLoader(AAset(selection='all'), batch_size=4, shuffle=True)
+    dataloader = torch_geometric.data.DataLoader(
+        AAset(selection="all"), batch_size=4, shuffle=True
+    )
     batch = next(iter(dataloader))
-    print (batch.input_element.device)
+    print(batch.input_element.device)
     batch.to("cuda")
-    #out = batch_to_device(batch, "cuda")
-    print (batch.input_element.device)
-    #print (out.input_element.device)
+    # out = batch_to_device(batch, "cuda")
+    print(batch.input_element.device)
+    # print (out.input_element.device)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
--- practice.py
+++ practice.py
@@ -6,10 +6,11 @@ from typing import Optional, List
 import torch
 import torch.nn as nn
 import numpy as np
+
 np.set_printoptions(suppress=True, linewidth=1000)
 
 import torch_geometric
-import torch_cluster 
+import torch_cluster
 import torch_scatter
 
 import e3nn
@@ -20,19 +21,22 @@ from libdata import AAset
 from liblayer import loss_f_soft as loss_f
 from liblayer import TestModel, test
 
+
 def main():
     trainloader = torch_geometric.loader.DataLoader(AAset(), batch_size=4, shuffle=True)
-    testloader = torch_geometric.loader.DataLoader(AAset(), batch_size=20, shuffle=False)
+    testloader = torch_geometric.loader.DataLoader(
+        AAset(), batch_size=20, shuffle=False
+    )
     #
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    #model = TestModel(layer='SE3Transformer').to(device)
-    model = TestModel(layer='ConvLayer').to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # model = TestModel(layer='SE3Transformer').to(device)
+    model = TestModel(layer="ConvLayer").to(device)
     model.device = device
     #
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
     test(model, testloader)
     for epoch in range(250):
-        loss_sum = 0.
+        loss_sum = 0.0
         model.train()
         for batch in trainloader:
             optimizer.zero_grad()
@@ -47,12 +51,12 @@ def main():
             #
             optimizer.step()
 
-        if (epoch+1)%50 == 0:
-            print ("LOSS ", epoch, loss_sum)
+        if (epoch + 1) % 50 == 0:
+            print("LOSS ", epoch, loss_sum)
             test(model, testloader)
-    print (epoch, loss_sum)
+    print(epoch, loss_sum)
     test(model, testloader)
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
